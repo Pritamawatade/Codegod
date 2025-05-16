d995c6b6336891cc1553d5f2f13e7b8b00f218d8
pritamawatade <pritamawatade5@gmail.com>
2025-05-16 12:38:27 +0530

    add authentication pages and components with form validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 function App() {
   return (
     <div className="flex flex-col items-center justify-start">
       <Routes>
         <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
